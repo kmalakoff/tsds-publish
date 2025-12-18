2be51786aeb8c02563f42c844f8ae494c1994142
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 21:13:46 -0800

    Update callback return types from undefined to void

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,2 @@
 export type HasChangedResult = { changed: boolean; reason: string };
-export type HasChangedCallback = (error?: Error, result?: HasChangedResult) => undefined;
+export type HasChangedCallback = (error?: Error, result?: HasChangedResult) => void;
