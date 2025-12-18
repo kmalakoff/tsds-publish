b2d3811f57873bb2ab643d0254f7f192501375d4
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-18 01:32:49 -0800

    Upgrade modules

--- src/lib/hasChanged.ts
+++ src/lib/hasChanged.ts
@@ -1,19 +1,17 @@
 import Module from 'module';
-import { wrap } from 'node-version-call';
+import { bind } from 'node-version-call';
 import path from 'path';
 import type { CommandOptions } from 'tsds-lib';
 import url from 'url';
 
 const major = +process.versions.node.split('.')[0];
-const version = major >= 18 ? 'local' : 'stable';
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
 const dist = path.join(__dirname, '..', '..');
-const workerWrapper = wrap(path.join(dist, 'cjs', 'lib', 'hasChanged.js'));
 
 import type { HasChangedCallback } from '../types.ts';
 
-function worker(options: CommandOptions, callback: HasChangedCallback) {
+function run(options: CommandOptions, callback: HasChangedCallback) {
   const cwd: string = (options.cwd as string) || process.cwd();
   const { needsPublish } = _require('npm-needs-publish');
 
@@ -27,6 +25,8 @@ function worker(options: CommandOptions, callback: HasChangedCallback) {
     .catch(callback);
 }
 
+const worker = major >= 20 ? run : bind('>=20', path.join(dist, 'cjs', 'lib', 'hasChanged.js'), { callbacks: true });
+
 export default function hasChanged(options: CommandOptions, callback: HasChangedCallback): void {
-  version !== 'local' ? workerWrapper(version, options, callback) : worker(options, callback);
+  worker(options, callback);
 }
